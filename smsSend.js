4e8e83c372e4090020f5f1ffd82fa336f89453dd
Richard Kondwani <92359432+kondwanidickson@users.noreply.github.com>
2023-06-17 07:19:56 -0700

    Create smsSend.js

---
+++ smsSend.js
@@ -0,0 +1,44 @@
+const axios = require("axios");
+const https = require("https");
+var btoa = require("btoa");
+
+const api_key = "<api_key>";
+const secret_key = "<secret_key>";
+const content_type = "application/json";
+const source_addr ="<source_addr>";
+
+function send_sms() {
+  axios
+    .post(
+      "https://apisms.beem.africa/v1/send",
+      {
+        source_addr: source_addr,
+        schedule_time: "",
+        encoding: 0,
+        message: "Hello World",
+        recipients: [
+          {
+            recipient_id: 1,
+            dest_addr: "255700000001",
+          },
+          {
+            recipient_id: 2,
+            dest_addr: "255700000002",
+          },
+        ],
+      },
+      {
+        headers: {
+          "Content-Type": content_type,
+          Authorization: "Basic " + btoa(api_key + ":" + secret_key),
+        },
+        httpsAgent: new https.Agent({
+          rejectUnauthorized: false,
+        }),
+      }
+    )
+    .then((response) => console.log(response, api_key + ":" + secret_key))
+    .catch((error) => console.error(error.response.data));
+}
+
+send_sms();
